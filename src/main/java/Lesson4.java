6dd3c8a2c6e6b4e915f980c1dbfa01ff178ad92d
KirillSorokin1986 <kir.a.sorokin@gmail.com>
2021-09-27 01:27:33 +0500

    Проверка на победу сделана циклами

--- src/main/java/Lesson4.java
+++ src/main/java/Lesson4.java
@@ -2,152 +2,177 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-    public class Lesson4 {
+public class Lesson4 {
 
-        private static char[][] map;
-        private static final char X_DOT = 'X';
-        private static final char O_DOT = 'O';
-        private static final char EMPTY_DOT = '•';
-        private static final int mapSize = 3;
-        private static final Scanner SC = new Scanner(System.in);
+    private static char[][] map;
+    private static final char X_DOT = 'X';
+    private static final char O_DOT = 'O';
+    private static final char EMPTY_DOT = '•';
+    private static final int mapSize = 3;
+    private static final Scanner SC = new Scanner(System.in);
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            initMap();
+        initMap();
+        printMap();
+
+        while (true) {
+            humanTurn();
             printMap();
-
-            while (true) {
-                humanTurn();
-                printMap();
-                if (isWin(X_DOT)) {
-                    System.out.println("YOU WIN");
-                    break;
-                }
-                if (isDraw()) {
-                    break;
-                }
-                computerTurn();
-                printMap();
-                if (isWin(O_DOT)) {
-                    System.out.println("YOU LOSE");
-                    break;
-                }
-                if (isDraw()) {
-                    break;
-                }
+            if (isWin(X_DOT)) {
+                System.out.println("YOU WIN");
+                break;
             }
-
+            if (isDraw()) {
+                break;
+            }
+            computerTurn();
+            printMap();
+            if (isWin(O_DOT)) {
+                System.out.println("YOU LOSE");
+                break;
+            }
+            if (isDraw()) {
+                break;
+            }
         }
 
-        private static boolean isWin(char dot) {
-            if (map[0][0] == dot && map[0][1] == dot && map[0][2] == dot){
-                return true;
-            }
-            if (map[1][0] == dot && map[1][1] == dot && map[1][2] == dot){
-                return true;
-            }
-            if (map[2][0] == dot && map[2][1] == dot && map[2][2] == dot){
-                return true;
-            }
+    }
 
-            if (map[0][0] == dot && map[1][0] == dot && map[2][0] == dot){
+    private static boolean isWin(char dot) {
+        int checkDotCount = 0;
+        for (int i = 0; i < map.length; i++) {
+            checkDotCount = 0;
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j] != dot) {
+                    break;
+                }
+                checkDotCount++;
+            }
+            if (checkDotCount == mapSize) {
                 return true;
             }
-            if (map[0][1] == dot && map[1][1] == dot && map[2][1] == dot){
-                return true;
-            }
-            if (map[0][2] == dot && map[1][2] == dot && map[2][2] == dot){
-                return true;
-            }
-
-            if (map[0][0] == dot && map[1][1] == dot && map[2][2] == dot){
-                return true;
-            }
-            if (map[2][0] == dot && map[1][1] == dot && map[0][2] == dot){
-                return true;
-            }
-
-            return false;
         }
 
 
-        private static void humanTurn() {
-            int xCoordinate;
-            int yCoordinate;
-            System.out.println("Введите координаты в формате \"x пробел y\"");
-            do {
-                xCoordinate = -1;
-                yCoordinate = -1;
-
-                if (SC.hasNextInt()) {
-                    xCoordinate = SC.nextInt();
+        for (int i = 0; i < map.length; i++) {
+            checkDotCount = 0;
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[j][i] != dot) {
+                    break;
                 }
-                if (SC.hasNextInt()) {
-                    yCoordinate = SC.nextInt();
-                }
-                SC.nextLine();
-            } while (!isValidHumanTurn(xCoordinate, yCoordinate));
-        }
-
-        private static void computerTurn() {
-            int xCoordinate;
-            int yCoordinate;
-            Random random = new Random();
-            do {
-                xCoordinate = random.nextInt(mapSize);
-                yCoordinate = random.nextInt(mapSize);
-            } while (!isValidComputerTurn(xCoordinate, yCoordinate));
-        }
-
-        private static boolean isValidHumanTurn(int xCoordinate, int yCoordinate) {
-            if (xCoordinate < 1 || yCoordinate < 1 ||
-                    xCoordinate > mapSize || yCoordinate > mapSize) {
-                System.out.println("Вы ввели неправильные координаты. Введите координаты в формате \"x пробел y\"");
-                return false;
+                checkDotCount++;
             }
-
-            if (map[xCoordinate - 1][yCoordinate - 1] == EMPTY_DOT) {
-                map[xCoordinate - 1][yCoordinate - 1] = X_DOT;
+            if (checkDotCount == mapSize) {
                 return true;
             }
+        }
+        int checkDotCountLine = 0;
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j] == dot && i == j) {
+                    checkDotCountLine++;
+                }
+                if (checkDotCountLine == mapSize) {
+                    return true;
+                }
+            }
+        }
+        int checkDotCountLineTwo = 0;
+        int lastJ = mapSize - 1;
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j] == dot && j == lastJ) {
+                    lastJ = lastJ - 1;
+                    checkDotCountLineTwo++;
+                }
+                if (checkDotCountLineTwo == mapSize) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    private static void humanTurn() {
+        int xCoordinate;
+        int yCoordinate;
+        System.out.println("Введите координаты в формате \"x пробел y\"");
+        do {
+            xCoordinate = -1;
+            yCoordinate = -1;
+
+            if (SC.hasNextInt()) {
+                xCoordinate = SC.nextInt();
+            }
+            if (SC.hasNextInt()) {
+                yCoordinate = SC.nextInt();
+            }
+            SC.nextLine();
+        } while (!isValidHumanTurn(xCoordinate, yCoordinate));
+    }
+
+    private static void computerTurn() {
+        int xCoordinate;
+        int yCoordinate;
+        Random random = new Random();
+        do {
+            xCoordinate = random.nextInt(mapSize);
+            yCoordinate = random.nextInt(mapSize);
+        } while (!isValidComputerTurn(xCoordinate, yCoordinate));
+    }
+
+    private static boolean isValidHumanTurn(int xCoordinate, int yCoordinate) {
+        if (xCoordinate < 1 || yCoordinate < 1 ||
+                xCoordinate > mapSize || yCoordinate > mapSize) {
             System.out.println("Вы ввели неправильные координаты. Введите координаты в формате \"x пробел y\"");
             return false;
         }
 
-        private static boolean isValidComputerTurn(int xCoordinate, int yCoordinate) {
-            if (map[xCoordinate][yCoordinate] == EMPTY_DOT) {
-                map[xCoordinate][yCoordinate] = O_DOT;
-                return true;
-            }
-            return false;
-        }
-
-        private static void printMap() {
-            for (int i = 0; i < map.length; i++) {
-                for (int j = 0; j < map[i].length; j++) {
-                    System.out.print(map[i][j] + " ");
-                }
-                System.out.println();
-            }
-        }
-
-        private static void initMap() {
-            map = new char[mapSize][mapSize];
-            for (int i = 0; i < mapSize; i++) {
-                Arrays.fill(map[i], EMPTY_DOT);
-            }
-        }
-
-        private static boolean isDraw() {
-            for (int i = 0; i < map.length; i++) {
-                for (int j = 0; j < map[i].length; j++) {
-                    if (map[i][j] == EMPTY_DOT) {
-                        return false;
-                    }
-                }
-            }
-            System.out.println("DRAW");
+        if (map[xCoordinate - 1][yCoordinate - 1] == EMPTY_DOT) {
+            map[xCoordinate - 1][yCoordinate - 1] = X_DOT;
             return true;
         }
+        System.out.println("Вы ввели неправильные координаты. Введите координаты в формате \"x пробел y\"");
+        return false;
     }
+
+    private static boolean isValidComputerTurn(int xCoordinate, int yCoordinate) {
+        if (map[xCoordinate][yCoordinate] == EMPTY_DOT) {
+            map[xCoordinate][yCoordinate] = O_DOT;
+            return true;
+        }
+        return false;
+    }
+
+    private static void printMap() {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void initMap() {
+        map = new char[mapSize][mapSize];
+        for (int i = 0; i < mapSize; i++) {
+            Arrays.fill(map[i], EMPTY_DOT);
+        }
+    }
+
+    private static boolean isDraw() {
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                if (map[i][j] == EMPTY_DOT) {
+                    return false;
+                }
+            }
+        }
+        System.out.println("DRAW");
+        return true;
+    }
+}
 
